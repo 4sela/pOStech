f656dc5031585cc23938e70d42844cb889fd7a39
4sela <adawa21@student.sdu.dk>
2025-09-29 19:44:31 +0900

    added more values in thread struct

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,12 +88,14 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
-    int64_t wakeup_tick;                /* Tick to wake up thread */
+    int original_priority;              /* Original priority before donation */
+    int wakeup_tick;                    /* Tick to wake up thread */
     struct list_elem allelem;           /* List element for all threads list. */
     struct list_elem sleep_elem;        /* List element for sleeping threads list. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    struct list_elem ready_elem;        /* List element for ready queue */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
